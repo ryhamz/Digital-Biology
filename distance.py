2cd358cbd4a94c1d1937ac3de95fc6766538aa72
ryhamz <ryhamz@gmail.com>
2015-04-09 01:47:30 -0500

    Removed old function. Added documentation of functionality

--- distance.py
+++ distance.py
@@ -7,49 +7,9 @@ import matplotlib.pyplot as plt
 from numpy.random import normal
 import pylab as P
 
-
-
-
-
-
-def analyse2(name, f):
-	parser=PDBParser()
-
-	#structure=parser.get_structure('1NTI', '1NTI.pdb')
-	structure=parser.get_structure(name, f)
-	dbl=PDBList()
-	dbl.retrieve_pdb_file(name)
-	count = 1;
-	ds = []
-	temp = 0;
-	for model in structure:
-	 for chain in model:
-	  for residue in chain:
-	   for atom in residue:
-	    if atom.get_name() == 'CA':
-			if count > 1:
-			 diff = abs(atom.get_coord() - temp);
-			 square = diff*diff;
-			 s = sum(square);
-			 ds.append(s)
-			 
-			 
-			 temp = atom.get_coord()
-			else:
-			 temp = atom.get_coord(); count += 1
-	print ds	 
-	print np.mean(ds)	
-	print np.min(ds)
-	print ds.index(np.min(ds))	
-	print np.max(ds)
-	print ds.index(np.max(ds))	
-	
-	plt.hist(ds, bins = 300, range = (0,20), histtype = 'step')
-	plt.title("Distances between alpha carbons")
-	plt.xlabel("Value")
-	plt.ylabel("Frequency")
-	plt.show()
-	
+#Takes in a protein name and protein data bank file name and returns
+#information on the distance distribution of the protein's C-alpha carbons.
+#Returns the mean distance, min, max, as well as a histogram.
 def analyse(name, f):
 	parser=PDBParser()
 
@@ -60,17 +20,18 @@ def analyse(name, f):
 	count = 1;
 	ds = []
 	temp = 0;
+	#Iterate through each atom
 	for model in structure:
 	 for chain in model:
 	  for residue in chain:
 	   for atom in residue:
-	    if atom.get_name() == 'CA':
-			if count > 1:
+	    if atom.get_name() == 'CA': #Checks if the atom is C-alpha
+			if count > 1: #If we are past the first atom, calculate the distance between the current and prior atom.
 			 dist = atom - temp
 			 ds.append(dist)
 			 temp = atom
 			 count +=1
-			else:
+			else: #Wait for second atom.
 			 temp = atom; count += 1
 			 		 
 	print np.mean(ds)	
@@ -79,6 +40,7 @@ def analyse(name, f):
 	print np.max(ds)
 	print ds.index(np.max(ds))	
 	
+	#Creates histogram of the distribution
 	plt.hist(ds, bins = 300, range = (2.5,4.5), histtype = 'step')
 	plt.title("Distances between alpha carbons")
 	plt.xlabel("Value")
